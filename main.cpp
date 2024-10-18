10eac42106aee844883aed39f67354f9445deb73
llvvv <llvvvnnn@gmail.com>
2024-10-19 01:01:39 +0300

    Хрень (пытался сделать A* )

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 
-#include <chrono>
 #include <random>
 #include <vector>
 #include <string>
 #include <curses.h>
 #include <my_map.h>
 #include "my_boy.h"
+#include <set>
+#include <vector>
 
 
 class monster{
@@ -13,11 +14,144 @@ public:
 
     int x;
     int y;
-    monster() {}
-    void move_monster() {
 
+    int width;
+    int length;
+
+
+    monster(int spx, int spy, int width_of_area, int length_of_area) : x(spx), y(spy), width(width_of_area), length(length_of_area) {}
+
+    void move_monster(int boy_x, int boy_y, char** map) {
+
+        point boy(boy_y, boy_x);
+        point active_point(x,y);
+
+        std::set<point> open_list;
+        std::vector<point> way ;
+
+       /* way.push_back(active_point);*/
+        active_point.move = 0;
+
+        while (!(boy >> active_point)) {
+           
+
+            if (active_point.x + 1 < width && map[active_point.y][active_point.x + 1] == ' ') {
+                point p = active_point;
+
+                p.x++;
+                if (check_in_way(way, p)) {
+                    p.move = 10 + active_point.move;
+                    p.weight = p.move + 10*(abs(boy.x - p.x) + abs(boy.y - p.y));
+
+                    open_list.insert(p);
+                    
+              
+                }
+                /*pow(boy.x - p.x, 2) + pow(boy.y - p.y, 2*/
+                
+            }
+            if (0 <= active_point.x - 1 && map[active_point.y][active_point.x - 1] == ' ') {
+                point p = active_point;
+
+                p.x--;
+            
+                if (check_in_way(way, p)) {
+                    p.move = 10 + active_point.move;
+                    p.weight = p.move + 10 * (abs(boy.x - p.x) + abs(boy.y - p.y));
+
+                    open_list.insert(p);
+                    
+                    
+                }
+
+            }
+            if (active_point.y + 1 < length && map[active_point.y+1][active_point.x] == ' ' ) {
+                point p = active_point;
+
+                p.y++;
+               
+                if (check_in_way(way, p)) {
+                    p.move = 10 + active_point.move;
+                    p.weight = p.move + 10 * (abs(boy.x - p.x) + abs(boy.y - p.y));
+
+                    open_list.insert(p);
+                   
+                    
+                }
+
+            }
+            if (0 <= active_point.y - 1 && map[active_point.y-1][active_point.x] == ' ') {
+                point p = active_point; 
+                p.y--;
+
+                if (check_in_way(way, p)) {
+                    p.move = 10 + active_point.move;
+                    p.weight = p.move + 10 * (abs(boy.x - p.x) + abs(boy.y - p.y));
+
+                    open_list.insert(p);
+                    
+                    
+                }
+            }
+            way.push_back(active_point);
+            active_point = *open_list.begin();
+            open_list.clear();
+            
+        }
+        for (const auto& p : way) {
+            move(p.x, p.y);
+            printw("w");
+        }
     }
+
 private:
+
+    struct point
+    {
+        public:
+            point(int X, int Y ) : x(X), y(Y) {}
+
+            int x;
+            int y;
+
+            int move;
+            int weight;
+
+            bool operator>>(const point& other) const {
+
+                return x == other.x && y == other.y;
+
+            }
+
+            bool operator<(const point& other) const {
+
+                if (weight < other.weight)
+                    return true;
+                if (weight == other.weight && weight < other.weight)
+                    return true;
+                return false;
+            }
+
+    };
+    bool check_in_way(std::vector<point> way, point point) {
+        if (way.size() == 0)
+            return true;
+
+        for (const auto& p : way)
+            if (p >> point)
+                return false;
+            else return true;
+    }
+
+  /*  std::set<point>PointAdd(point active_point, point p, point boy, openlist ) {
+
+        p.cfx = active_point.x;
+        p.cfy = active_point.y;
+        p.move = 10 + active_point.move;
+        p.weight = p.move + pow(boy.x - p.x, 2) + pow(boy.y - p.y, 2);
+        open_list.insert(p);
+        
+    }*/
 
 };
 int main()
@@ -40,6 +174,7 @@ int main()
     map.show_map();
 
     my_boy boy(start_pos_x,start_pos_y);
+    monster W(5, 5, width, length);
 
     while (true) {
         switch (getch()) {
@@ -74,6 +209,7 @@ int main()
 
         map.show_map();
         boy.move_boy(map.forest);
+        W.move_monster(boy.x,boy.y,map.forest);
         
        /* refresh();*/
         
